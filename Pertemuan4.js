4f3266404e9c57fbf25f1dd5d1fcb25e0d3ff911
SanliPatras <69838916+SanliPatras@users.noreply.github.com>
2020-09-10 17:44:24 +0800

    Add files via upload

---
+++ Pertemuan4.js
@@ -0,0 +1,21 @@
+/*
+    1. Function
+*/
+
+const penjumlahanDuaBujurSangkar = function (a, b) {
+	return a * a + b * b;
+};
+
+console.log(penjumlahanDuaBujurSangkar (8, 3));
+console.log(penjumlahanDuaBujurSangkar (10, 5));
+
+//console.log(tambah(kali(5, 2), kali(10, 2)));
+
+//function tambah(a, b) {
+	//return a + b;
+//}
+//function tambah(a, b) {
+	//return a * b;
+//}
+
+
