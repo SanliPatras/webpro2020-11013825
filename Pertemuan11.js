5dd4bc56d223b8f2af6dd3def5dada3449e47f34
SanliPatras <69838916+SanliPatras@users.noreply.github.com>
2020-09-29 15:06:35 +0800

    Add files via upload

---
+++ Pertemuan11.js
@@ -0,0 +1,62 @@
+//ARROW FUNCTION
+//Bukan Arrow Function
+//var ucapkanSalam = function () {
+//	return "Selamat Pagi";
+//}
+//console.log(ucapkanSalam());
+
+//Arrow Function
+//var ucapkanSalam = ( ) => "Selamat Pagi";
+//console.log(ucapkanSalam());
+
+//Contoh yang lain dari Arrow Function
+//const operasiPenjumlahan = (bilangan1, bilangan2) => {
+//	const hasil = bilangan1 + bilangan2;
+//	return hasil;
+//};
+//console.log(operasiPenjumlahan (5, 9));
+
+//Multiple Arguments
+//const calcAge = (birthYear, currenYear) //=> {
+//	const x;
+//	return currenYear - birthYear;
+//};
+
+//No argument with inplicit return value
+//const calcAge = ( ) => 2020 - 1992;
+//console.log(calcAge());
+
+// Bukan Arrow Function
+//const year = [1990, 1992, 2021, 2005];
+//const calcAge5 = year.map(function (el) {
+//	return 2020 - el;
+//});
+
+// Arrow Function
+//const calcAge6 = year.map((el) => 2020 - el);
+//console.log(calcAge6);
+
+// EXERCISE 03 (Arrow Funtion)
+//Convert to ES6 
+
+// ES5 SYNTAX
+//function yearUntilRetirement(year, firstName) {
+//	var age = 28;
+//	var retirement = 65 - age;
+//	if (retirement > 0) {
+//		console.log(firstName + "retired in" + retirement + "year");
+//	}else {
+//		console.log(firstName + "is already //retired.");
+//	}
+//}
+
+//ES6 SYNTAX
+const yearUntilRetirement = (year, firstName) => {
+	var age = 28;
+	var retirement = 65 - age;
+	if (retirement > 0) {
+		console.log(firstName + "retired in" + retirement + "year");
+	}else {
+		console.log(firstName + "is already retired.");
+	}
+}
