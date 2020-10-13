358824bcd008b6bb61b6dddd97abfa4e92504afe
SanliPatras <69838916+SanliPatras@users.noreply.github.com>
2020-10-13 13:55:58 +0800

    Add files via upload

---
+++ helloPertemuan16.js
@@ -0,0 +1,9 @@
+export const numbers = [1, 2, 3, 4, 5];
+
+export const hello = (uers) => {
+	console.log('hello, ${user}');
+}
+
+export const world = ( ) => {
+	console.log("Hello World");
+}
---
+++ indexPertemuan16.js
@@ -0,0 +1,7 @@
+import {hello, world, numbers} from './helloPertemuan16.js'
+
+import * as helper from './hello.js'
+
+helper.hello('John Doe');
+helper.world( );
+console.log(helper.numbers)
